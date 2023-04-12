fda692c463a28c3eb7e4249949734d9e6fa28d6c
sarubito <nagaoaustineo@gmail.com>
2023-04-12 11:55:57 +0900

    add retun in calculation function

--- src/demo.cpp
+++ src/demo.cpp
@@ -9,7 +9,7 @@ namespace nakanosima
         signal_pub_ = create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 1);
         using namespace std::chrono_literals;
         current_pose_subscription_ = create_subscription<geometry_msgs::msg::PoseStamped>("current_pose",10, std::bind(&Demo::callback, this, std::placeholders::_1));
-        pose.position_x = 10.0;
+        pose.position_x = 2.0;
         pose.position_y = 0.0;
         pose.orientation_z = 0.0;
         pose.state = 0;
@@ -23,6 +23,8 @@ namespace nakanosima
         float y = data->pose.position.y;
         float timeout = 10.0;
         geometry_msgs::msg::Twist signal = calcurate_velocity(x, y, pose, timeout);
+        RCLCPP_INFO(this->get_logger(), "linear x : %f", signal.linear.x);
+        RCLCPP_INFO(this->get_logger(), "linear y : %f", signal.linear.y);
         signal_pub_->publish(signal);
     }
 
@@ -33,6 +35,7 @@ namespace nakanosima
         distance = std::sqrt(std::pow(x - pose.position_x, 2.0)+std::pow(y - pose.position_y, 2.0));
         RCLCPP_INFO(this->get_logger(), "distance : %f", distance);
         RCLCPP_INFO(this->get_logger(), "pose.position_x : %f", pose.position_x);
+        RCLCPP_INFO(this->get_logger(), "state : %d", pose.state);
         if(pose.state == 0){
             if(distance <= 0.1){
                 data.linear.x = 0.0;
@@ -43,7 +46,7 @@ namespace nakanosima
                 data.angular.z = 1.5;
                 pose.state++;
             } else {
-                data.linear.x = 0.1;
+                data.linear.x = 0.2;
                 data.linear.y = 0.0;
                 data.linear.z = 0.0;
                 data.angular.x = 0.0;
@@ -61,7 +64,7 @@ namespace nakanosima
                 data.angular.z = 1.5;
                 pose.state++;
             } else {
-                data.linear.x = 0.1;
+                data.linear.x = 0.2;
                 data.linear.y = 0.0;
                 data.linear.z = 0.0;
                 data.angular.x = 0.0;
@@ -78,7 +81,7 @@ namespace nakanosima
                 data.angular.z = 1.5;
                 pose.state++;
             } else {
-                data.linear.x = 0.1;
+                data.linear.x = 0.2;
                 data.linear.y = 0.0;
                 data.linear.z = 0.0;
                 data.angular.x = 0.0;
@@ -95,7 +98,7 @@ namespace nakanosima
                 data.angular.z = 1.5;
                 pose.state++;
             } else {
-                data.linear.x = 0.1;
+                data.linear.x = 0.2;
                 data.linear.y = 0.0;
                 data.linear.z = 0.0;
                 data.angular.x = 0.0;
@@ -109,7 +112,8 @@ namespace nakanosima
             data.angular.x = 0.0;
             data.angular.y = 0.0;
             data.angular.z = 0.0;
-        } 
+        }
+        return data; 
     }
 
     Demo::~Demo(){}
